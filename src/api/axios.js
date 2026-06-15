4cc37f76aa1f71e5edc8ca46c99ae7b1080fb441
admin-agat <admin.it.agat@gmail.com>
2026-06-15 15:27:36 -0500

    fix: usar VITE_API_URL para produccion

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// En local usa Laravel local, en producción usa Render automáticamente
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", //conecta con laravel local
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:8000/api",
 });
 
 // Agrega el token automáticamente en cada request
